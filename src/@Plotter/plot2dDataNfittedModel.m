14b1e4dd704de58d0b7a6931a48b172b58a81a53
Nuno Guedelha <nuno.guedelha@iit.it>
2017-07-12 03:17:55 +0200

    Add all plotters

---
+++ src/@Plotter/plot2dDataNfittedModel.m
@@ -0,0 +1,30 @@
+function plot2dDataNfittedModel(...
+    figuresHandler,aTitle,aLabel,...
+    xData,yData,xModel,yModel,...
+    xLabel,yLabel,...
+    dataLegend,modelLegend)
+%UNTITLED9 Summary of this function goes here
+%   Detailed explanation goes here
+
+%% Plot joint trajectories
+figH = figure('Name',aTitle,'WindowStyle', 'docked');
+
+figuresHandler.addFigure(figH,aLabel); % Add figure to the figure handler
+
+% If the figure is not docked, use the below command to display it full
+% screen.
+%set(gcf,'PositionMode','manual','Units','normalized','outerposition',[0 0 1 1]);
+title(aTitle,'Fontsize',16,'FontWeight','bold');
+hold on
+
+scatter(xData,yData,10,'blue','filled');
+plot(xModel,yModel,'r','lineWidth',4.0);
+
+hold off
+grid ON;
+xlabel('Time (sec)','Fontsize',12);
+ylabel(yLabel,'Fontsize',12);
+legend('Location','BestOutside',y1Legend,y2Legend);
+set(gca,'FontSize',12);
+
+end
